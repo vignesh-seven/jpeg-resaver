323ba856c2bf47bc1f6cfc6cbc3d623d79d23362
Vignesh <vignesh77024@gmail.com>
2021-08-01 16:55:18 +0530

    Feature: Incompatible file handling is added

--- main.py
+++ main.py
@@ -18,16 +18,15 @@ def convert(img_input):
     final_image.save(os.path.abspath(os.path.join(new_dirname, just_filename) + "_resaved.jpg"), "JPEG")
     print("saved: " + os.path.abspath(os.path.join(new_dirname, just_filename) + "_resaved.jpg"))
 
+
 def change_info_text():
-    #threading.Timer(3.0, change_info_text)
-    time.sleep(3)
     info_text = "Drag files here"
-    area.itemconfig(text_on_canvas, text=info_text)
+    drag_area.configure(text=info_text, fg="black")
 
     
 # triggered on drag n drop
 def start(event):   
-    files = ws.tk.splitlist(event.data)
+    files = root.tk.splitlist(event.data)
     # print(files)
     # some path variables related to the image (for future features)    
     dirname = os.path.dirname(files[0])
@@ -49,26 +48,30 @@ def start(event):
         os.mkdir(os.path.abspath(full_new_dirname))
         # print("make folder is CHECKED\n" + full_new_dirname)
         new_dirname = os.path.abspath(full_new_dirname)
-
+    index = 0
     for filename in files:
         kind = filetype.guess(filename)
-        #print(kind.mime)
-        if kind.mime == "image/jpeg":
+        print(kind.mime)
+        if kind.mime == "image/jpeg" or "image/png" or "image/tiff":
+            drag_area.configure(text=f"Processing\n{index + 1} of {len(files)}", fg="blue")
         #if filename.endswith(".jpeg") or filename.endswith(".jpg"):
             print("The file is valid!")
             convert(filename)
             if (deleteInputImages.get() == 1):
                 os.remove(filename)
+            drag_area.after(500, change_info_text)
         else:
             print("INVALID FILE TYPE: " + kind.mime)
-            info_text = "Invalid File Type!"
-            area.itemconfig(text_on_canvas, text=info_text)
-            change_info_text()
+            info_text = "File not valid!"
+            drag_area.configure(text=info_text, fg="red")
+            drag_area.after(3000, change_info_text)
+        index += 1
             
             
         #print("The file path: " + filename)
 
     print("All Done!")
+    #change_text_info()
         
 if __name__ == "__main__":
     from tkinter import *
@@ -87,61 +90,69 @@ if __name__ == "__main__":
     window_height = 350
 
     # Window config
-    ws = TkinterDnD.Tk()
-    ws.title("JPEG Resaver")
-    ws.geometry(str(window_width) + "x" + str(window_height) + "+500+50")
-    # ws.pack()
-    ws.resizable(True, False)
+    root = TkinterDnD.Tk()
+    root.title("JPEG Resaver")
+    #setting window size
+    width=400
+    height=350
+    screenwidth = root.winfo_screenwidth()
+    screenheight = root.winfo_screenheight()
+    alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+    root.geometry(alignstr)
+    #root.resizable(width=False, height=False)
+    root.rowconfigure(0, weight=10)
+    root.rowconfigure(1, weight=1)
+    root.columnconfigure(0, weight=1)
+    root.columnconfigure(1, weight=1)
     
-    frameForDrag = Frame(ws)
-    frameForDrag.pack()
+    #frameForDrag = Frame(root, width=window_width, height=window_height*0.85)
+    #frameForDrag.pack()
 
-    frameForOptions = Frame(ws, height=window_height*0.15, bg = "green")
-    frameForOptions.pack(side=tkinter.BOTTOM, fill=tkinter.Y)
+    #frameForOptions = Frame(root, height=window_height*0.15, bg = "green")
+    #frameForOptions.pack(side=tkinter.BOTTOM, fill=tkinter.Y)
 
-    # Drop area config
+    # Drop drag_area config
     global info_text
     info_text = "Drag files here"
     
-    area = Canvas(frameForDrag,
-                  width=window_width,
-                  height=window_height*0.85,
+    drag_area = Label(root,
+                  text = info_text,
+                  font = "Arial 18 italic",
+                  justify="center",
                   bg="#cccccc")
-    text_on_canvas = area.create_text(window_width / 2,
-                     window_height / 2,
-                     fill="#303030",
-                     font="Arial 18 italic", text=info_text)
-    area.pack(side=LEFT)
-    area.drop_target_register(DND_FILES)
-    area.dnd_bind('<<Drop>>', start)
-    area.pack()
-
-    #area.itemconfig(text_on_canvas, text=status_text)
+    #text_on_canvas = drag_area.create_text(window_width / 2,
+    #                 window_height / 2,
+    #                 fill="#303030",
+    #                 font="Arial 18 italic", text=info_text)
+    drag_area.pack(side=LEFT)
+    drag_area.drop_target_register(DND_FILES)
+    drag_area.dnd_bind('<<Drop>>', start)
+    #drag_area.pack(fill=BOTH, expand=True)
+    drag_area.grid(column=0, row=0, columnspan=2, rowspan=1, sticky="EWNS")
+    
+    
+    #drag_area.itemconfig(text_on_canvas, text=status_text)
 
     global makeAFolder
     makeAFolder = IntVar() 
-    FolderCheckBox = Checkbutton(frameForOptions, text = "Make a folder", 
+    FolderCheckBox = Checkbutton(root, text = "Make a folder", 
                           variable = makeAFolder,
-                          #height = int(window_height*0.15),
-                          width = 25,
-
-                          #expand=True
-                                 )
-    FolderCheckBox.pack(side=tkinter.LEFT, fill=tkinter.Y, expand=False)
+                          #bg = "red",
+                          justify="center",
+                          )
+    FolderCheckBox.grid(column=0, row=1, columnspan=1, rowspan=1, sticky="EWNS")
 
     global deleteInputImages
     deleteInputImages = IntVar() 
-    DeleteSourceCheckBox = Checkbutton(frameForOptions, text = "Delete input files", 
+    DeleteSourceCheckBox = Checkbutton(root, text = "Delete input files", 
                           variable = deleteInputImages,
-                          height = int(window_height*0.1),
-                          width = 25,
-                          #bg = "blue"
-                          #expand=True
-                                 )
-    DeleteSourceCheckBox.pack(side=tkinter.LEFT, fill=tkinter.Y, expand=False)
+                          #bg = "blue",
+                          justify="center"
+                          )
+    DeleteSourceCheckBox.grid(column=1, row=1, columnspan=1, rowspan=1, sticky="EWNS")
 
     
-    ws.mainloop()
+    root.mainloop()
 
 
 
